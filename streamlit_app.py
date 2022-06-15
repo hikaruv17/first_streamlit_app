e10eeb319c54122eb367720509a36c53296dcea8
Hikaru Sakuno <107536767+hikaruv17@users.noreply.github.com>
2022-06-15 16:30:17 +0900

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Memu')
 streamlit.text('Omega3 & blueberry Oatmal')
-streamlit.text('kale, Spinach & Rocket moothie ')
+streamlit.text('kale, Spinach & Rocket smoothie ')
 streamlit.text('Hard-Boiled Free-Range Egg')
