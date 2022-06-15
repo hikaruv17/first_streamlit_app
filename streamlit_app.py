3f0e1cea91bddcf890607eb6bb396d947f402e63
Hikaru Sakuno <107536767+hikaruv17@users.noreply.github.com>
2022-06-15 16:39:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv（ "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"）
+streamlit.dataframe（my_fruit_list）
