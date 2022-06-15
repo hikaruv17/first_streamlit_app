ee83fc882cdf3a5103d26cb484ded66f91650fe2
Hikaru Sakuno <107536767+hikaruv17@users.noreply.github.com>
2022-06-15 16:34:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('My Mon's New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega3 & blueberry Oatmal')
-streamlit.text('🥗kale, Spinach & Rocket smoothie ')
+streamlit.text('🥗kale, Spinach & Rocket smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado toast')
