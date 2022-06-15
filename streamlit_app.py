aaa09b580e0988cbd3852364e7152d8b77758664
Hikaru Sakuno <107536767+hikaruv17@users.noreply.github.com>
2022-06-15 16:33:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mon's New Healthy Diner')
 
 
-streamlit.header('Breakfast Memu')
-streamlit.text('Omega3 & blueberry Oatmal')
-streamlit.text('kale, Spinach & Rocket smoothie ')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega3 & blueberry Oatmal')
+streamlit.text('🥗kale, Spinach & Rocket smoothie ')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado toast')
