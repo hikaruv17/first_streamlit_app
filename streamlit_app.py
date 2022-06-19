51ea7d193cb4ffbffcf815cd6462a56eaf85a368
Hikaru Sakuno <107536767+hikaruv17@users.noreply.github.com>
2022-06-19 17:45:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,9 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruity Advice!')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
 
 
 furityvice_nomalized = pandas.json_nomalize(fruityvice_response.json())
-
 streamlit.dataframe(furityvice_nomalized)
